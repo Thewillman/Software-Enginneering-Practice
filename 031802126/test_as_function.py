87e4e88d0c10f6395a7dd93f71707d4d206a3998
Thewillman <1137399580@qq.com>
2020-09-15 00:04:28 +0800

    test_as_function add lsi and lda

--- 031802126/test_as_function.py
+++ 031802126/test_as_function.py
@@ -5,6 +5,67 @@ import test#引用一下计算程序，不然代码属实太长
 from gensim import corpora, models, similarities
 import os
 import time
+
+def cal_similarity_lsi(orig_items, orig_sim_items, array):
+    # 对原始文本用gensim库中的doc2bow和corpora进行处理，采用tfidf模型
+    # 生成词典
+    dictionary = corpora.Dictionary(orig_items)
+    # 通过doc2bow稀疏向量生成语料库
+    corpus = [dictionary.doc2bow(item) for item in orig_items]
+    # 通过TF模型算法，计算出tf值
+    lsi = models.LsiModel(corpus)
+    # 通过token2id得到特征数（字典里面的键的个数）
+    num_features = len(dictionary.token2id.keys())
+    # 计算稀疏矩阵相似度，建立一个索引
+    index = similarities.MatrixSimilarity(lsi[corpus], num_features=num_features)
+
+    # 开始对抄袭文本的相似度进行计算
+    ans = 0.0
+    for i in range(0, len(orig_sim_items)):
+        # 把每个分好词的句子建立成新的稀疏向量并代入模型计算相似度
+        orig_sim_vec = dictionary.doc2bow(orig_sim_items[i])
+        sim = index[lsi[orig_sim_vec]]
+        sim_max = max(sim)
+        if sim_max < 0.0025:  # 对于相似度低于0.25%的句子我们直接视为不相关
+            continue
+        try:
+            ans += max(sim) * array[i]  # 显然我们这里要取最高相似度而不是一一对应，可能会有下标超界的错误
+        except IndexError:
+            print("orz下标超界了")
+        else:
+            continue
+    return ans
+
+def cal_similarity_lda(orig_items, orig_sim_items, array):
+    # 对原始文本用gensim库中的doc2bow和corpora进行处理，采用tfidf模型
+    # 生成词典
+    dictionary = corpora.Dictionary(orig_items)
+    # 通过doc2bow稀疏向量生成语料库
+    corpus = [dictionary.doc2bow(item) for item in orig_items]
+    # 通过TF模型算法，计算出tf值
+    lda = models.LdaModel(corpus)
+    # 通过token2id得到特征数（字典里面的键的个数）
+    num_features = len(dictionary.token2id.keys())
+    # 计算稀疏矩阵相似度，建立一个索引
+    index = similarities.MatrixSimilarity(lda[corpus], num_features=num_features)
+
+    # 开始对抄袭文本的相似度进行计算
+    ans = 0.0
+    for i in range(0, len(orig_sim_items)):
+        # 把每个分好词的句子建立成新的稀疏向量并代入模型计算相似度
+        orig_sim_vec = dictionary.doc2bow(orig_sim_items[i])
+        sim = index[lda[orig_sim_vec]]
+        sim_max = max(sim)
+        if sim_max < 0.0025:  # 对于相似度低于0.25%的句子我们直接视为不相关
+            continue
+        try:
+            ans += max(sim) * array[i]  # 显然我们这里要取最高相似度而不是一一对应，可能会有下标超界的错误
+        except IndexError:
+            print("orz下标超界了")
+        else:
+            continue
+    return ans
+
 def orig_solve(orig_position):
     orig = open(orig_position, 'r', encoding='UTF-8')
     orig_text = orig.read()
@@ -21,16 +82,31 @@ def text_solve(text_position):
     text_items = test.create_jieba_list(test_text)
     array = test.cal_sentence_weight(test_text)
     return text_items,array
+
 def write_dir(ans,ans_position):
     str1 = str('0.2f' %ans)
     ans_text = open(ans_position,'w',encoding='UTF-8')
     ans_text.write(str1)
     ans_text.close()
     #print('0')
-def solve(orig_position,text_position,ans_position):
+
+def solve_tfidf(orig_position,text_position,ans_position):
     orig_items = orig_solve(orig_position)
     text_items,array = text_solve(text_position)
-    ans = test.cal_similarity(orig_items,text_items,array)
+    ans = test.cal_similarity_tfidf(orig_items,text_items,array)
     write_dir(ans,ans_position)
-    print('orz查重结果为%.2f' %ans)
+    print('orzTF-IDF模型查重结果为%.2f' %ans)
 
+def solve_lda(orig_position,text_position,ans_position):
+    orig_items = orig_solve(orig_position)
+    text_items,array = text_solve(text_position)
+    ans = cal_similarity_lda(orig_items,text_items,array)
+    write_dir(ans,ans_position)
+    print('orzLDA模型查重结果为%.2f' %ans)
+
+def solve_lsi(orig_position,text_position,ans_position):
+    orig_items = orig_solve(orig_position)
+    text_items,array = text_solve(text_position)
+    ans = cal_similarity_lsi(orig_items,text_items,array)
+    write_dir(ans,ans_position)
+    print('orzLSI模型查重结果为%.2f' %ans)
--- 031802126/unit_test.py
+++ 031802126/unit_test.py
@@ -2,7 +2,7 @@ import unittest
 import test_as_function  # 引用一下计算程序，不然代码属实太长
 from BeautifulReport import BeautifulReport
 
-class TestForAllText(unittest.TestCase):
+class TestForAllTextTfIdf(unittest.TestCase):
     @classmethod
     def setUp(self):
         print("开始单元测试……")
@@ -10,107 +10,115 @@ class TestForAllText(unittest.TestCase):
     def tearDown(self):
         print("已结束测试")
 
-    def test_self(self):
+    def test_self_tfidf(self):
         print("正在载入orig.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig.txt', 'ans.txt')
 
-    def test_add(self):
+    def test_add_tfidf(self):
         print("正在载入orig_0.8_add.txt")
-        test_as_function.solve('sim_0.8\orig.txt','sim_0.8\orig_0.8_add.txt','ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt','sim_0.8\orig_0.8_add.txt','ans.txt')
 
-    def test_del(self):
+    def test_del_tfidf(self):
         print("正在载入orig_0.8_del.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_del.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_del.txt', 'ans.txt')
 
-    def test_dis_1(self):
+    def test_dis_1_tfidf(self):
         print("正在载入orig_0.8_dis_1.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_1.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_1.txt', 'ans.txt')
 
-    def test_dis_3(self):
+    def test_dis_3_tfidf(self):
         print("正在载入orig_0.8_dis_3.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_3.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_3.txt', 'ans.txt')
 
-    def test_dis_7(self):
+    def test_dis_7_tfidf(self):
         print("正在载入orig_0.8_dis_7.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_7.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_7.txt', 'ans.txt')
 
-    def test_dis_10(self):
+    def test_dis_10_tfidf(self):
         print("正在载入orig_0.8_dis_10.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_10.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_10.txt', 'ans.txt')
 
-    def test_dis_15(self):
+    def test_dis_15_tfidf(self):
         print("正在载入orig_0.8_dis_15.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_15.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_dis_15.txt', 'ans.txt')
 
-    def test_mix(self):
+    def test_mix_tfidf(self):
         print("正在载入orig_0.8_mix.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_mix.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_mix.txt', 'ans.txt')
 
-    def test_rep(self):
+    def test_rep_tfidf(self):
         print("正在载入orig_0.8_rep.txt")
-        test_as_function.solve('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_rep.txt', 'ans.txt')
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_rep.txt', 'ans.txt')
+
+    def test_rep_tfidf_NoChineseError(self):
+        print("正在载入orig_NoChinese.txt")
+        test_as_function.solve_tfidf('sim_0.8\orig.txt', 'sim_0.8\orig_0.8_rep.txt', 'ans.txt')
+
+    def test_rep_tfidf_TextSameError(self):
+        print("正在载入")
 
     def test_My_database1_add(self):
         print("正在载入My_database1\\orig_add.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_add.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_add.txt', 'ans.txt')
 
     def test_My_database1_del(self):
         print("正在载入My_database1\\orig_del.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_del.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_del.txt', 'ans.txt')
 
     def test_My_database1_dis_1(self):
         print("正在载入My_database1\\orig_dis_1.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_dis_1.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_dis_1.txt', 'ans.txt')
 
     def test_My_database1_dis_3(self):
         print("正在载入My_database1\\orig_dis_3.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_dis_3.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_dis_3.txt', 'ans.txt')
 
     def test_My_database1_dis_7(self):
         print("正在载入My_database1\\orig_dis_7.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_dis_7.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_dis_7.txt', 'ans.txt')
 
     def test_My_database1_dis_10(self):
         print("正在载入My_database1\\orig_dis_10.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_dis_10.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_dis_10.txt', 'ans.txt')
 
     def test_My_database1_dis_15(self):
         print("正在载入My_database1\\orig_dis_15.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_dis_15.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_dis_15.txt', 'ans.txt')
 
     def test_My_database1_mix(self):
         print("正在载入My_database1\\orig_mix.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_mix.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_mix.txt', 'ans.txt')
 
     def test_My_database1_rep(self):
         print("正在载入My_database1\\orig_rep.txt")
-        test_as_function.solve('My_database1\orig.txt', 'My_database1\orig_rep.txt', 'ans.txt')
+        test_as_function.solve_tfidf('My_database1\orig.txt', 'My_database1\orig_rep.txt', 'ans.txt')
+
 
 if __name__ == '__main__':
     #unittest.main()
     suite = unittest.TestSuite()
-    suite.addTest(TestForAllText('test_self'))
-    suite.addTest(TestForAllText('test_add'))
-    suite.addTest(TestForAllText('test_del'))
-    suite.addTest(TestForAllText('test_dis_1'))
-    suite.addTest(TestForAllText('test_dis_3'))
-    suite.addTest(TestForAllText('test_dis_7'))
-    suite.addTest(TestForAllText('test_dis_10'))
-    suite.addTest(TestForAllText('test_dis_15'))
-    suite.addTest(TestForAllText('test_mix'))
-    suite.addTest(TestForAllText('test_rep'))
-    suite.addTest(TestForAllText('test_My_database1_add'))
-    suite.addTest(TestForAllText('test_My_database1_del'))
-    suite.addTest(TestForAllText('test_My_database1_dis_1'))
-    suite.addTest(TestForAllText('test_My_database1_dis_3'))
-    suite.addTest(TestForAllText('test_My_database1_dis_7'))
-    suite.addTest(TestForAllText('test_My_database1_dis_10'))
-    suite.addTest(TestForAllText('test_My_database1_dis_15'))
-    suite.addTest(TestForAllText('test_My_database1_mix'))
-    suite.addTest(TestForAllText('test_My_database1_rep'))
+    suite.addTest(TestForAllTextTfIdf('test_self_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_add_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_del_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_dis_1_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_dis_3_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_dis_7_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_dis_10_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_dis_15_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_mix_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_rep_tfidf'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_add'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_del'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_dis_1'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_dis_3'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_dis_7'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_dis_10'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_dis_15'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_mix'))
+    suite.addTest(TestForAllTextTfIdf('test_My_database1_rep'))
     runner = BeautifulReport(suite)
     runner.report(
         description='论文查重测试报告',  # => 报告描述
-        filename='nlp.html',  # => 生成的报告文件名
+        filename='nlp_TFIDF.html',  # => 生成的报告文件名
         log_path='.'  # => 报告路径
     )
